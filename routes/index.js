4af1987c9717b1e0faf78be997af7f6a88cd5692
Houss82 <houssem1982mansour@gmail.com>
2023-11-15 17:16:02 +0100

    mymovizf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const keyAPI = process.env.OWM_API_KEY;
 
 
-// route get ajouté
+// deniere modif
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${keyAPI}`)
     .then(response => response.json())
